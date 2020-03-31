bf8d7f39af4bf9f96dfb572977561a8ed22aa490
Fendy Candra Nugraha <fendysketsa@gmail.com>
2020-03-31 10:46:03 +0700

    adding new file

---
+++ deploy.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Project name
+set('application', 'gula akunting');
+
+// Project repository
+set('repository', 'git@gitlab.com:layanacorp/akunting.git');
+set('branch', 'master');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true);
+
+//set('writable_mode', 'chown');
+
+// Shared files/dirs between deploys
+add('shared_files', []);
+add('shared_dirs', []);
+
+// Writable dirs by web server
+add('writable_dirs', []);
+set('http_user', 'admin');
+
+// Hosts
+
+host('68.183.182.57')
+    ->user('admin')
+    ->identityFile('/home/layana/.ssh/gula_server_rsa')
+    ->set('deploy_path', '/home/admin/web/akunting.gulawaxing.com/public_html');
+
+// Tasks
+
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+ 
+// Migrate database before symlink new release.
+ 
+//before('deploy:symlink', 'artisan:migrate');
---
+++ resources/lang/nl-NL/demo.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+
+    'accounts' => [
+        'cash'              => 'Contant geld',
+    ],
+
+    'categories' => [
+        'deposit'           => 'Storting',
+        'sales'             => 'Omzet',
+    ],
+
+    'currencies' => [
+        'usd'               => 'Amerikaanse Dollar',
+        'eur'               => 'Euro',
+        'gbp'               => 'Britse pond',
+        'try'               => 'Turkse Lira',
+    ],
+
+    'offline_payments' => [
+        'cash'              => 'Cash',
+        'bank'              => 'Bank Transfer',
+    ],
+
+    'reports' => [
+        'income'            => 'Monthly income summary by category.',
+        'expense'           => 'Monthly expense summary by category.',
+        'income_expense'    => 'Monthly income vs expense by category.',
+        'tax'               => 'Quarterly tax summary.',
+        'profit_loss'       => 'Quarterly profit & loss by category.',
+    ],
+
+];
